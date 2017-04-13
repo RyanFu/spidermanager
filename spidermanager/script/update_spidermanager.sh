60efd9239aaa9cd1b30f7608c765b19cb9daf9b6
taoyang <hackty@163.com>
2017-04-13 11:37:12 +0800

    alter env 4 test

---
+++ spidermanager/script/update_spidermanager.sh
@@ -0,0 +1,6 @@
+rm -rf ~/spidermanager/server
+rm -rf ~/spidermanager/server
+wget https://github.com/hackty/spidermanager/archive/master.zip
+unzip master.zip
+mv spidermanager-master ~/spidermanager/server
+rm master.zip
