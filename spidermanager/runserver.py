81b1436aa7918f823b30ca93eb326c4a0217a68f
taoyang <hackty@163.com>
2017-04-19 18:15:44 +0800

    update for test env

--- spidermanager/runserver.py
+++ spidermanager/runserver.py
@@ -5,7 +5,7 @@ from spidermanager.controller import main, user
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=False)
 
 
 def main():
